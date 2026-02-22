6553a4efad47bfde39b5923c8548eeec06bff110
dhayalamit <amitdhayal6138@gmail.com>
2026-02-23 00:41:55 +0530

    updated

---
+++ sem 2/class-14/react-intro/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import ArrayMethods from "./components/ArrayMethods";
+// import Home from "./components/Home";
+
+function App() {
+  //code here
+  return <>
+  {/* <Home/> */}
+  <ArrayMethods/>
+  </>;
+}
+
+export default App;
